a54d6f89ed1435f049a7166ab7dc8b0743aceeb2
Nikola Sretkovic <nsretkovic23@gmail.com>
2023-12-17 02:39:48 +0100

    Create Ball class

---
+++ Assets/Scripts/Base/Ball.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Base;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [field:SerializeField] public Field CurrentField { get; set; }
+}
