4ebacdd0662221eab77a90dec37a055227a3aae2
Nikola Sretkovic <nsretkovic23@gmail.com>
2023-12-17 02:57:01 +0100

    Implement methods for checking if player can move or pass to the field

--- Assets/Scripts/Modes/Standard/Field.cs
+++ Assets/Scripts/Modes/Standard/Field.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Modes.Standard
 {
     public class Field : Base.Field
     {
-        // Start is called before the first frame update
-        void Start()
+        // TODO: Remove ball spaghetti dependency between Field and Ball
+        public override bool CanMoveToTheField(Base.Player player)
         {
-
+            bool result =  Players.Count < 2
+                   && !IsForbidden
+                   && !IsPlayerOnTheField(player)
+                   && Players.TrueForAll(p => p.Manager != player.Manager)
+                   && ((Ball == null && Players.Count > 0) || IsEmpty);
+            
+            return result;
         }
-
-        // Update is called once per frame
-        void Update()
+        
+        public override bool CanPassTheBallToTheField(Base.Player player)
         {
-
+            bool result =
+                !IsForbidden
+                && !IsGoalkeeper
+                && Players.Count == 1 // TODO: If decided that ball can be passed to the empty field modify to (Players.Count == 1 || IsEmpty)
+                && Players[0].Manager == player.Manager
+                && Players[0] != player;
+            
+            return result;
         }
     }
 }
